448f1d34f530e89a3772c8854ed6b8b8f99a4769
Lévy <prakkmak@gmail.com>
2025-01-17 15:48:31 +0100

    :wrench: Add netlify compatibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
+    output: 'server',
+    adapter: netlify({
+      edgeMiddleware: true
+    }),
     integrations: [
       starlight({
         title: 'My delightful docs site',
